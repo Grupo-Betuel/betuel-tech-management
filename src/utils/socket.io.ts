7d86e1ec3a6a30565912409c7e31d8eaaa4faa30
Williams <wilfre_padilla@hotmail.com>
2022-09-01 19:07:08 -0400

    klk

--- src/utils/socket.io.ts
+++ src/utils/socket.io.ts
@@ -1,7 +1,8 @@
 import { Socket } from "socket.io-client";
 
 export const DEV_SOCKET_URL = 'http://localhost:5000'
-export const PROD_SOCKET_URL = 'http://165.232.158.16'
+// export const PROD_SOCKET_URL = 'http://165.232.158.16'
+export const PROD_SOCKET_URL = 'https://www.betuel-promotions.xyz'
 export const CONNECTED_EVENT = 'connect'
 export const onSocketOnce = (socket: Socket, eventName: string, callback: (data: any) => any) => {
     if (!socket.hasListeners(eventName)) {
